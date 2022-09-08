735294b963771003f8ac5505c1175f386d415964
Antonio <antonio18casta@aol.com>
2022-09-07 22:29:49 -0400

    second commit

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,10 @@
-const catPhoto = document.getElementById("cat-photo")
-const nameCatBtn = document.getElementById("nameBtn")
+const catPhoto = document.getElementById("cat-photo");
+let cats = []
 document.addEventListener("DOMContentLoaded", () => {
     fetchRandomCatPic();
     nameCat();
     toggleDarkMode();
+    getNextCat();
  });
 
 
@@ -12,23 +13,38 @@ const fetchRandomCatPic = () => {
         .then(resp => resp.json())
         .then(data => {
             catPhoto.innerHTML = `<img src="${data[0].url}"/>`
+            appendCatList(data);
         })
-}
+};
+
+function getNextCat() {
+    const input = document.getElementById("getNextCat");
+    input.addEventListener("click", fetchRandomCatPic);
+};
 
 function nameCat() {
     document.getElementById("name-cat-form").addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const cat = document.getElementById("yourName")
+        const cat = document.getElementById("yourName");
         const name = document.createElement("h1");
         name.textContent = e.target.description.value;
         cat.append(name);
     })
-}
+};
+
+function appendCatList(cats) {
+    const ul = document.getElementById("list");
+    cats.map(cat => {
+        const li = document.createElement("li")
+        li.textContent = cat
+        ul.append(li)
+    })
+};
 
 function toggleDarkMode() {
     const checkbox = document.getElementById("checkbox");
     checkbox.addEventListener("change", () => {
         document.body.classList.toggle("dark");
     })
-}
+};
